c9a30e05152030ffbd8667fc1d0fac816732095a
VitorPandini <valvespandini@gmail.com>
2025-11-01 15:06:03 -0300

    feat : add endpoint for create order and response

---
+++ order-service-api/src/main/java/br/com/vitorpandini/orderserviceapi/controllers/OrderController.java
@@ -0,0 +1,54 @@
+package br.com.vitorpandini.orderserviceapi.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import models.exceptions.StandardError;
+import models.requests.AuthenticateRequest;
+import models.requests.CreateOrderRequest;
+import models.responses.AuthenticateResponse;
+import models.responses.OrderResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/orders")
+@Tag(name= "OrderController", description = "Controller responsible for orders operations")
+public interface OrderController {
+
+    @Operation(summary = "Save and persist new order")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Order created"),
+
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = StandardError.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "Not Found",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = StandardError.class))
+            ),
+
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = StandardError.class))
+            ),
+
+
+    }
+    )
+    @PostMapping()
+    ResponseEntity<Void> saveOrder(@Valid @RequestBody final CreateOrderRequest request) ;
+
+}
